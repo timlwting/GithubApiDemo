87693082a9480bd45cff0afe022ca6d3c81e50ed
timliu <timlwting@proApple.local>
2015-10-06 22:33:08 +0800

    1. Added loading UI to calendar view

--- OptimisPT/CalendarViewController.m
+++ OptimisPT/CalendarViewController.m
@@ -61,7 +61,18 @@
 
 - (void)getGitEvent
 {
+    __block UIAlertView *loadingView;
+    dispatch_async(dispatch_get_main_queue(), ^{
+        loadingView = [[UIAlertView alloc] initWithTitle:@"Loading ..." message:nil delegate:nil cancelButtonTitle:nil otherButtonTitles:nil];
+        [loadingView show];
+    });
+
     [AppUtility getDataFrom:[NSString stringWithFormat:@"https://api.github.com/repos/%@/%@/commits", self.gListData[@"owner"][@"login"], self.gListData[@"name"]]  completion:^(NSDictionary *dict, NSError *error) {
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [loadingView dismissWithClickedButtonIndex:0 animated:YES];
+        });
+
         if ([dict isKindOfClass:[NSArray class]]) {
             NSArray *responseArray = (NSArray *)dict;
             if (!self.dateSet) {
@@ -72,16 +83,20 @@
                 self.dateFormatter = [[NSDateFormatter alloc] init];
                 [self.dateFormatter setDateFormat:@"yyyy-MM-dd'T'HH:mm:ss'Z'"];
             }
-            [responseArray enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-                
-                
-                NSDateComponents *components = [[NSCalendar currentCalendar] components:NSCalendarUnitYear|NSCalendarUnitMonth|NSCalendarUnitDay
-                                                                               fromDate:[self.dateFormatter dateFromString:obj[@"commit"][@"author"][@"date"]]];
-                
-                [self.dateSet addObject:[[NSCalendar currentCalendar] dateFromComponents:components]];
-                
-                
-            }];
+            @try {
+                [responseArray enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+                    
+                    
+                    NSDateComponents *components = [[NSCalendar currentCalendar] components:NSCalendarUnitYear|NSCalendarUnitMonth|NSCalendarUnitDay
+                                                                                   fromDate:[self.dateFormatter dateFromString:obj[@"commit"][@"author"][@"date"]]];
+                    
+                    [self.dateSet addObject:[[NSCalendar currentCalendar] dateFromComponents:components]];
+                    
+                    
+                }];
+            }
+            @catch (NSException *exception) {
+            }
             dispatch_async(dispatch_get_main_queue(), ^{
                 [self.calendar reloadData];
             });
